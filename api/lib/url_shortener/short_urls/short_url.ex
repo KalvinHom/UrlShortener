787ba134641c12746e5aef8a23e8c8a041cbe81a
Kalvin Hom <kalvin.hom@gmail.com>
2021-10-12 16:30:19 -0700

    add changeset uri validation

--- api/lib/url_shortener/short_urls/short_url.ex
+++ api/lib/url_shortener/short_urls/short_url.ex
@@ -1,17 +1,40 @@
 defmodule UrlShortener.ShortUrls.ShortUrl do
-    use Ecto.Schema
-    import Ecto.Changeset
-    @derive {Jason.Encoder, only: [:url, :slug]}
+  use Ecto.Schema
+  import Ecto.Changeset
+  @derive {Jason.Encoder, only: [:url, :slug]}
 
-    schema "short_urls" do
-        field :url, :string
-        field :slug, :string
+  schema "short_urls" do
+    field(:url, :string)
+    field(:slug, :string)
+  end
+
+  def changeset(short_url, attrs) do
+    short_url
+    |> cast(attrs, [:url, :slug])
+    |> validate_required([:url, :slug])
+    |> validate_change(:url, &validate_url/2)
+  end
+
+  defp validate_url(field, value) do
+    value
+    |> URI.parse
+    |> validate_scheme
+    |> validate_host
+    |> case do
+      :error -> [{:url, "Invalid URL"}]
+      _ -> []
     end
+  end
 
-    def changeset(short_url, attrs) do
-        short_url
-        |> cast(attrs, [:url, :slug])
-        |> validate_required([:url, :slug])
+  defp validate_scheme(%URI{scheme: scheme} = uri) when scheme == "http" or scheme == "https",
+    do: uri
+
+  defp validate_scheme(_), do: :error
+
+  defp validate_host(%URI{host: host} = uri) do
+    case :inet.gethostbyname(Kernel.to_charlist host) do
+        {:ok, _} -> uri
+        {:error, _} -> :error
     end
-
+  end
 end
--- api/lib/url_shortener_web/controllers/fallback_controller.ex
+++ api/lib/url_shortener_web/controllers/fallback_controller.ex
@@ -19,23 +19,4 @@ defmodule UrlShortenerWeb.FallbackController do
     |> put_view(UrlShortenerWeb.ErrorView)
     |> render(:"404")
   end
-
-  def call(conn, {:error, errors}) when is_list(errors) do
-    conn
-    |> put_status(400)
-    |> json(%{errors: errors})
-  end
-
-  def call(conn, {:error, errors}) do
-    conn
-    |> put_status(400)
-    |> json(%{errors: [errors]})
-  end
-
-  def call(conn, nil) do
-    conn
-    |> put_status(:not_found)
-    |> put_view(UrlShortenerWeb.ErrorView)
-    |> render(:"404")
-  end
 end
